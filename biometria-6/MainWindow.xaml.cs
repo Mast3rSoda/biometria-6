b1cdb590d0ddaa0fe1409e3643c8435bb866ddb1
michal.boltowicz <micbo26@gmail.com>
2022-04-27 00:54:36 +0200

    reading data

--- biometria-6/MainWindow.xaml.cs
+++ biometria-6/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,48 @@ namespace biometria_6
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public int SliderValue
+        {
+            get => sliderValue;
+            set
+            {
+                sliderValue = value;
+                PropertyChanged?.Invoke(this, new(nameof(SliderValue)));
+            }
+        }
+
+        private int sliderValue;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.txt;)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                using (FileStream fs = File.OpenRead(fileName))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        stringBuilder.Append(temp.GetString(b));
+                    }
+                    ReadData.Text=stringBuilder.ToString();
+                }
+            }
+        }
+       
     }
 }
